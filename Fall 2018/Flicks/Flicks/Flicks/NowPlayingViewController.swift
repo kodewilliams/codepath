51099483b1621d0352d28a445c3ae917638df65b
kodewilliams <kode.a.williams@gmail.com>
2019-02-11 01:09:10 -0800

    Restructured files.

---
+++ Fall 2018/Flicks/Flicks/Flicks/NowPlayingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NowPlayingViewController.swift
+//  Flicks
+//
+//  Created by Kode Williams on 8/15/18.
+//  Copyright © 2018 Kode Williams. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Make network request
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This runs when the network request runs.
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
